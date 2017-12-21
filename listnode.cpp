3af9054f6d89ec0b92685f2e8e583c7e0fef1d54
kimichang <kimichangsign@gmail.com>
2017-12-21 22:14:14 +0800

    update

--- listnode.cpp
+++ listnode.cpp
@@ -9,14 +9,21 @@ struct ListNode{
 
 ListNode * removeElements(ListNode* head,int val){
 	ListNode * p = head;
+	ListNode tmp = NULL;
 	while(p){
 		if(val == p->val){
-			p = p->next;
+			tmp = *p;
+			if(p->next == NULL)
+			  return NULL ;
+			*p = *(p->next);
+			tmp = NULL;
 			//p->next = (p->next)->next;
 			//p = p->next;
 		}
 		else if(val == p->next->val)
 		{   
+			if(p->next == NULL)
+			  return NULL;
 			p->next = p->next->next;
 			p = p->next;
 							   
@@ -28,23 +35,26 @@ ListNode * removeElements(ListNode* head,int val){
 }
 
 int main(){
-	ListNode* a = new ListNode(1);
-	ListNode* b = new ListNode(2);
+	ListNode* a = new ListNode(5);
+//	ListNode* b = new ListNode(1);
+	/*
 	ListNode* c = new ListNode(2);
 	ListNode* d = new ListNode(3);
 	ListNode* e = new ListNode(3);
 	ListNode* f = new ListNode(4);
 	ListNode* g = new ListNode(3);
-
-	a->next = b;
+*/
+//	a->next = b;
+//	b->next = NULL;
+/*
 	b->next = c;
 	c->next = d;
 	d->next = e;
 	d->next = f;
 	f->next = g;
 	g->next = NULL;
-
-	ListNode * aaa = removeElements(a,3);
+*/
+	ListNode * aaa = removeElements(a,1);
 	while(aaa->next != NULL)
 	{
 		cout << aaa->val << endl;
